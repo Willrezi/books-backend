8887783808c42c53ef8f1950598ab9d7bd47201e
Wilfried LLOZA <wilfried.lloza@gmail.com>
2019-03-14 11:29:15 +0100

    add bookModel

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ app.get("/", function(req, res) {
   res.json({ message: "Hello API" });
 });
 
+//-----MODELS-----//
+
+// BOOK MODEL //
+const bookModel = mongoose.model("Book", {
+  title: String,
+  author: String,
+  editor: String,
+  country: String,
+  genre: String,
+  pages: Number,
+  note: Number,
+  read: String
+});
+
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server started");
 });
