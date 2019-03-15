f0ce46622002943255d0483a9a98bf3a4560aba2
Wilfried LLOZA <wilfried.lloza@gmail.com>
2019-03-15 12:13:08 +0100

    add validator package

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const validator = require("validator");
+const SHA256 = require("crypto-js/sha256");
+const encBase64 = require("crypto-js/enc-base64");
+const uid2 = require("uid2");
+
 const User = require("../models/User");
 
 module.exports = router;
