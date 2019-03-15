38212db5ff717011a57db17e2c7dc94a4c36b109
Wilfried LLOZA <wilfried.lloza@gmail.com>
2019-03-15 12:06:09 +0100

    route add book

--- routes/book.js
+++ routes/book.js
@@ -2,5 +2,23 @@ const express = require("express");
 const router = express.Router();
 
 const Book = require("../models/Book");
+const User = require("../models/User");
+
+router.post("/add", function(req, res) {
+  User.findOne({
+    token: req.headers.authorization.replace("Bearer ", "")
+  }).exec(function(err, userAuthenticated) {
+    if (userAuthenticated && !err) {
+      const book = new Book(req.body);
+      book.save(function(err, addedBook) {
+        if (!err) {
+          res.status(200).json(addedBook);
+        } else {
+          res.status(400).json("Error message", err.message);
+        }
+      });
+    }
+  });
+});
 
 module.exports = router;
