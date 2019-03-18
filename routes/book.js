6214f0f6ce865bc0891596ce68de9a923bb0fa89
Wilfried LLOZA <wilfried.lloza@gmail.com>
2019-03-18 16:21:25 +0100

    route add bike updated

--- routes/book.js
+++ routes/book.js
@@ -6,13 +6,27 @@ const Book = require("../models/Book");
 const User = require("../models/User");
 
 router.post("/add", isAuthenticated, function(req, res) {
-  const book = new Book(req.body);
-  console.log("body", req.body);
+  const obj = {
+    title: req.body.title,
+    author: req.body.author,
+    editor: req.body.editor,
+    country: req.body.country,
+    genre: req.body.genre,
+    pages: req.body.pages,
+    note: req.body.note,
+    read: req.body.read,
+    user: req.user
+  };
+
+  const book = new Book(obj);
+  console.log("body", obj);
 
   book.save(function(err, addedBook) {
     if (!err) {
-      //   user.save();
+      req.user.books.push(book._id);
+      req.user.save();
       res.status(200).json(addedBook);
+      console.log(addedBook);
     } else {
       res.status(400).json("Error message", err.message);
     }
